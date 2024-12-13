dbf7f3a86ad4f1d4458c8551f3540205073a2994
sibekov <vusimuzij.sibeko@gmail.com>
2024-12-13 15:14:46 +0000

    Add urls

--- firsttemplateproj/firsttemplateproj/settings.py
+++ firsttemplateproj/firsttemplateproj/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'firsttemplateapp'
+    'firsttemplateapp',
 ]
 
 MIDDLEWARE = [
